f4026060bdeb43826380396fa5f5fd5308498a54
Toan Pham <phamvantoan1993@gmail.com>
2018-10-10 11:15:02 +0700

    change env

--- video_expression/__init__.py
+++ video_expression/__init__.py
@@ -11,6 +11,6 @@ face_detector = dlib.get_frontal_face_detector()
 graph = tf.get_default_graph()
 
 model_emoji_64_64 = load_model(MODEL_EMOJI_64_64)
-model_emoji = model_from_json(open(MODEL_EMOJI_JSON, "r").read())
-
-model_emoji.load_weights(MODEL_EMOJI_WEIGHTS) #load weights
+# model_emoji = model_from_json(open(MODEL_EMOJI_JSON, "r").read())
+#
+# model_emoji.load_weights(MODEL_EMOJI_WEIGHTS) #load weights
